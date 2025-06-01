7dd51cc9efd6b6d70a79e4867c149b657a6e9b02
LAGM <luisiyi1997@gmail.com>
2025-06-01 16:48:33 -0700

    modificarTarea method added
    -added "modificarTarea" method, is for update a tarea selected from our table, if we click "Modificar" button without selecting an record from our table, it will send an error message.
    if everything is good, we create a new tarea object, we call "recolectarDatosFormulario" method, then we call "tareaServicio.guardarTarea(tarea)", then we show a message, we clear the field with "limpiarFormulario", and finally we list all the tareas.
    -updated "limpiarFormulario" method, now we set the idTareaInterno in null.
    -updated "recolectarDatosFormulario" method, now we validate the "idTareainterno" if is different of null, if it is we only set to our object the "idTarea" value equal to "idTareaInterno".
    -updated "agregarTarea" method, we confirm the tarea object´s id is set to null. this is for comparassing the adding or updating method.

--- src/main/java/gm/tareas/controlador/IndexControlador.java
+++ src/main/java/gm/tareas/controlador/IndexControlador.java
@@ -82,6 +82,7 @@ public class IndexControlador implements Initializable {
         else {
             var tarea = new Tarea();
             recolectarDatosFormulario(tarea);
+            tarea.setIdTarea(null);
             tareaServicio.guardarTarea(tarea);
             mostrarMensaje("Informacion", "Tarea agregada");
             limpiarFormulario();
@@ -100,12 +101,33 @@ public class IndexControlador implements Initializable {
     }
 
     private void recolectarDatosFormulario(Tarea tarea){
+        if (idTareaInterno != null)
+            tarea.setIdTarea(idTareaInterno);
         tarea.setNombreTarea(nombreTareaTexto.getText());
         tarea.setResponsable(responsableTexto.getText());
         tarea.setEstatus(estatusTexto.getText());
     }
 
+    public void modificarTarea(){
+        if(idTareaInterno == null){
+            mostrarMensaje("Informacion", "Debe seleccionar una tarea");
+            return;
+        }
+        if (nombreTareaTexto.getText().isEmpty()){
+            mostrarMensaje("Error Validación", "Debe proporcionar una tarea");
+            nombreTareaTexto.requestFocus();
+            return;
+        }
+        var tarea = new Tarea();
+        recolectarDatosFormulario(tarea);
+        tareaServicio.guardarTarea(tarea);
+        mostrarMensaje("Informacion", "Tarea modificada");
+        limpiarFormulario();
+        listarTareas();
+    }
+
     private void limpiarFormulario(){
+        idTareaInterno = null;
         nombreTareaTexto.clear();
         responsableTexto.clear();
         estatusTexto.clear();
